0ffd91c0e2b7133b905df3402c20d9bbe0b457aa
drinking <pan49@126.com>
2015-10-27 16:21:22 +0800

    Disable Scrolling when there is only on item

--- PXRotatorView.podspec
+++ PXRotatorView.podspec
@@ -9,7 +9,7 @@
 
 Pod::Spec.new do |s|
   s.name             = "PXRotatorView"
-  s.version          = "0.5.3"
+  s.version          = "0.5.4"
   s.summary          = "An auto rotating carousel view based on iCarousel and ReactiveCocoa"
   s.description      = <<-DESC
                         An carousel view based on iCarousel and ReactiveCocoa.
--- Pod/Classes/Models/PXRotatorBaseViewModel.m
+++ Pod/Classes/Models/PXRotatorBaseViewModel.m
@@ -11,36 +11,40 @@
 @implementation PXRotatorBaseViewModel
 
 - (instancetype)init {
-    if(self = [super init]){
+    if (self = [super init]) {
         _displayItems = [NSMutableArray array];
     }
     return self;
 }
 
-- (NSInteger)numberOfItemsInCarousel:(iCarousel *)carousel{
+- (NSInteger)numberOfItemsInCarousel:(iCarousel *)carousel {
     return [self.displayItems count];
 }
 
-- (UIView *)carousel:(iCarousel *)carousel viewForItemAtIndex:(NSInteger)index reusingView:(UIView *)view{
+- (UIView *)carousel:(iCarousel *)carousel viewForItemAtIndex:(NSInteger)index reusingView:(UIView *)view {
 
     @throw [NSException exceptionWithName:@"Provide carousel view"
                                    reason:@"You should override this method"
                                  userInfo:nil];
 }
 
-- (void)carouselCurrentItemIndexDidChange:(iCarousel *)carousel{
-    #pragma GCC diagnostic ignored "-Wundeclared-selector"
+- (void)carouselCurrentItemIndexDidChange:(iCarousel *)carousel {
+#pragma GCC diagnostic ignored "-Wundeclared-selector"
     if ([carousel.superview respondsToSelector:@selector(setPageConrolCurrentPage:)]) {
         [carousel.superview performSelector:@selector(setPageConrolCurrentPage:) withObject:@(carousel.currentItemIndex) afterDelay:0];
     }
 
 }
 
-- (CGFloat)carousel:(iCarousel *)carousel valueForOption:(iCarouselOption)option withDefault:(CGFloat)value{
+- (CGFloat)carousel:(iCarousel *)carousel valueForOption:(iCarouselOption)option withDefault:(CGFloat)value {
     if (option == iCarouselOptionWrap) {
         return YES;
     }
     return value;
+}
+
+- (void)carouselWillBeginDragging:(iCarousel *)carousel {
+    carousel.scrollEnabled = [self.displayItems count] > 1;
 }
 
 @end
