fd497f1b41d9acb0004b37d482a8215709b61d98
drinking <pan49@126.com>
2015-11-06 16:12:39 +0800

    Remove PureLayout

--- Example/PXRotatorView/PXViewController.m
+++ Example/PXRotatorView/PXViewController.m
@@ -9,56 +9,46 @@
 #import "PXViewController.h"
 #import "DKRotatorDemoViewModel.h"
 #import "PXRotatorView+StyledPageControl.h"
-#import <PXRotatorView/PXRotatorView.h>
 #import <PureLayout/ALView+PureLayout.h>
 
 @implementation PXViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
 
     self.navigationController.navigationBar.translucent = NO;
 
     _viewModel = [DKRotatorDemoViewModel new];
-    _viewModel.displayItems = [@[@"pic1.jpg",@"pic2.jpg",@"pic3.jpg",@"pic4.jpg"] mutableCopy];
+    _viewModel.displayItems = [@[@"pic1.jpg", @"pic2.jpg", @"pic3.jpg", @"pic4.jpg"] mutableCopy];
 
     _rotatorView = [[PXRotatorView alloc] init];
+    _rotatorView.frame = CGRectMake(0,0,CGRectGetWidth(self.view.frame),160);
     _rotatorView.interval = 1;
     [self.view addSubview:_rotatorView];
-    [_rotatorView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero excludingEdge:ALEdgeBottom];
-    [_rotatorView autoSetDimension:ALDimensionHeight toSize:160];
-
     [_rotatorView bindViewModel:self.viewModel];
     [_rotatorView addNumberPageControl];
     [_rotatorView updatePageControl];
 
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Rotate"
                                                                               style:UIBarButtonItemStylePlain
-                                                                             target:nil action:nil];
+                                                                             target:self
+                                                                             action:@selector(rotateOrStop)];
 
-    @weakify(self)
-    self.navigationItem.rightBarButtonItem.rac_command = [[RACCommand alloc] initWithSignalBlock:^RACSignal *(id input) {
-        @strongify(self)
-        if (self.rotatorView.isRotating){
-            [self.rotatorView stopRotating];
-        }else{
-            [self.rotatorView startRotating];
-        }
-
-        return [RACSignal empty];
-    }];
-
-    RAC(self.navigationItem.rightBarButtonItem,title) = [RACObserve(self.rotatorView, isRotating) map:^id(id rotating) {
-        return [rotating boolValue]?@"Stop":@"Rotate";
-    }];
-    
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)rotateOrStop {
+    if (self.rotatorView.isRotating) {
+        [self.navigationItem.rightBarButtonItem setTitle:@"Rotate"];
+        [self.rotatorView stopRotating];
+    } else {
+        [self.navigationItem.rightBarButtonItem setTitle:@"Stop"];
+        [self.rotatorView startRotating];
+    }
 }
 
 @end
